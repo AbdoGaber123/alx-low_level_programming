e908b2c3779b79ddf46d9a9810d18b03ad74b674
Abdallah_Gaber <112730560+AbdoGaber123@users.noreply.github.com>
2023-03-19 00:32:32 +0200

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+  
+int main()
+{
+  
+    // Get the character to be written
+    char ch = 'a';
+  
+    // Write the Character to stdout
+    for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+  
+    return (0);
+}
