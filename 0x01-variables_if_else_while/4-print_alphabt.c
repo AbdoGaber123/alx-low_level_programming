0938b81cb327c92f5af01db644bd6792e4fc3533
Abdallah_Gaber <112730560+AbdoGaber123@users.noreply.github.com>
2023-03-19 01:00:37 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,12 @@
  */
 int main()
 {
-  
-  char letter;
-
+char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 	if (letter != 'e' && letter != 'q')
 		putchar(letter);
 }
-
 putchar('\n');
 
 return (0);
