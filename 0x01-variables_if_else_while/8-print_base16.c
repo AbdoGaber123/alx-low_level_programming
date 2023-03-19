998b7e3e5300ddc2562b6d55c78877fd3f7bb452
Abdallah_Gaber <112730560+AbdoGaber123@users.noreply.github.com>
2023-03-19 10:22:49 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*
-    printing all numbers in base16 numbering system
-*/
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void){
     int num;
 	char letter;
